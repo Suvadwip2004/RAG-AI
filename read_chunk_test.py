e3ebef2a2d034e9abd5d864b881d56e9b990b57b
Suvadwip2004 <suvadwip14@gmail.com>
2025-12-15 14:13:36 +0530

    read Chunks is ready

--- read_chunk_test.py
+++ read_chunk_test.py
@@ -15,10 +15,32 @@ def create_embedding(text_list):
     return embedding
 
 jsons = os.listdir("jsons")
-
+my_dicts  = []
+chunk_id  = 0
 
 for json_file in jsons:
     with open(f"jsons/{json_file}","r") as f:
               content  = json.load(f)
     print(f"Creating embedding for {json_file}")
-    embeddings = 
+    embeddings = create_embedding([c['text'] for c in content['chunks']])
+    for i,chunk in enumerate(content['chunks']):
+          chunk['chunk_id'] = chunk_id
+          chunk_id +=1
+          chunk['embedding'] = embeddings[i]
+          my_dicts.append(chunk)
+
+    break
+
+df = pd.DataFrame.from_records(my_dicts)
+
+
+incoming_query  = input("Ask any question : ")
+question_embedding  = create_embedding([incoming_query])[0]
+
+similarityes  = cosine_similarity(np.vstack(df['embedding']),[question_embedding]).flatten()
+print(similarityes)
+top_result  = 3
+max_index  = similarityes.argsort()[::-1][0:top_result]
+print(max_index)
+new_df = df.loc[max_index]
+print(new_df[["title","number","text"]])
